9739adfddea332ceb2ed990ac92fa4f56f21fd02
Bogdan Shchennikov <b.shennikov@yandex.ru>
2026-08-21 00:29:15 +0300

    modified

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+if not exist build mkdir build
+cd build
+cmake ..
+cmake --build .
+ctest -C Debug --output-on-failure
+pause
---
+++ long_number/src/long_number.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+
+namespace biv {
+    class LongNumber {
+    private:
+        int* numbers;
+        int length;
+        int sign;
+
+    public:
+        LongNumber();
+        LongNumber(int length, int sign);
+        LongNumber(const char* const str);
+        LongNumber(const LongNumber& x);
+        LongNumber(LongNumber&& x);
+
+        ~LongNumber();
+
+        LongNumber& operator = (const char* const str);
+        LongNumber& operator = (const LongNumber& x);
+        LongNumber& operator = (LongNumber&& x);
+
+        bool operator == (const LongNumber& x) const;
+        bool operator != (const LongNumber& x) const;
+        bool operator > (const LongNumber& x) const;
+        bool operator < (const LongNumber& x) const;
+
+        LongNumber operator + (const LongNumber& x) const;
+        LongNumber operator - (const LongNumber& x) const;
+        LongNumber operator * (const LongNumber& x) const;
+        LongNumber operator / (const LongNumber& x) const;
+        LongNumber operator % (const LongNumber& x) const;
+
+        bool is_negative() const noexcept;
+
+        friend std::ostream& operator << (std::ostream &os, const LongNumber& x);
+
+    private:
+        int get_length(const char* const str) const noexcept;
+    };
+}
